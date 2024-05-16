bffdbc432d3d4af80bbbb736c618550c4b53df47
Pierre-Yves Goubet <pierre-yves.goubet@restena.lu>
2024-05-16 14:54:52 +0200

    Verify signature using algorithm defined in response
    - cosmetic changes

--- Kernel/System/Auth/SAML2SP/Response.pm
+++ Kernel/System/Auth/SAML2SP/Response.pm
@@ -21,10 +21,12 @@ use XML::LibXML;
 use Digest::SHA qw(sha1_base64 sha256_base64 sha512_base64);
 use Crypt::OpenSSL::RSA;
 use Crypt::OpenSSL::X509;
+use Switch;
+
 
 sub new{
 	my $class = shift;
-	my ( $raw_response, $settings ) = @_; 
+	my ( $raw_response, $settings ) = @_;
 	
 	my $self = bless {
 		raw_response => $raw_response,
@@ -35,7 +37,7 @@ sub new{
 		validated_nodes => undef,
 		signed_info => "",
 		key_sign_algo => ""
-	}, $class; 
+	}, $class;
 	
 	$self->decode();
 	
@@ -51,6 +53,7 @@ sub decode{
 	my $parser = XML::LibXML->new();
 	$self->{xml_as_dom} = $parser->parse_string($self->{xml_as_string});
 }
+
 
 sub is_valid{
 	my $self = shift;
@@ -86,10 +89,10 @@ sub is_valid{
 	}
 	
 	my $objKey = $self->locateKey();
-    if(! $objKey ) {
-        $Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "SAML: We have no idea about the key");
-        return;
-    }
+	if(! $objKey ) {
+			$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "SAML: We have no idea about the key");
+			return;
+	}
 	
 	if(!$self->verify_signature()){
 		$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "SAML: Signature verification failed !");
@@ -111,99 +114,99 @@ sub get_signature{
 	return $nodes[0];
 }
 
+
 sub canonicalizeSignedInfo() {
 	my $self = shift;
 	
-    my $canonicalmethod = undef;
-    
-    my $xpath = XML::LibXML::XPathContext->new($self->{signature_node});
-    $xpath->registerNs("secdsig", "http://www.w3.org/2000/09/xmldsig#");
-    my $query = "./secdsig:SignedInfo";
-    my @nodes = $xpath->findnodes($query);
-    my $signInfoNode = $nodes[0];
-    if ($signInfoNode) {
-    	my $xpath2 = XML::LibXML::XPathContext->new($signInfoNode);
-	    $xpath2->registerNs("secdsig", "http://www.w3.org/2000/09/xmldsig#");
-	    my $query2 = "./secdsig:CanonicalizationMethod";
-	    my @nodes2 = $xpath2->findnodes($query2);
-        my $canonNode = $nodes2[0];  
-        my @attr = $canonNode->attributes();
-        my $canonicalmethod;
-        for(my $j=0; $j < scalar(@attr) ; $j++){
-    		if($attr[$j]->nodeName eq "Algorithm"){
-    			$canonicalmethod = $attr[$j]->value;	
-    		}
-        }
-        $self->{signedInfo} = $self->canonicalizeData($signInfoNode, $canonicalmethod);
-        #$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "DEBUG: ".Dumper($self->{signedInfo}));
-        return $self->{signedInfo};
-    }
-    return;
+	my $canonicalmethod = undef;
+	
+	my $xpath = XML::LibXML::XPathContext->new($self->{signature_node});
+	$xpath->registerNs("secdsig", "http://www.w3.org/2000/09/xmldsig#");
+	my $query = "./secdsig:SignedInfo";
+	my @nodes = $xpath->findnodes($query);
+	my $signInfoNode = $nodes[0];
+	if ($signInfoNode) {
+		my $xpath2 = XML::LibXML::XPathContext->new($signInfoNode);
+		$xpath2->registerNs("secdsig", "http://www.w3.org/2000/09/xmldsig#");
+		my $query2 = "./secdsig:CanonicalizationMethod";
+		my @nodes2 = $xpath2->findnodes($query2);
+		my $canonNode = $nodes2[0];  
+		my @attr = $canonNode->attributes();
+		my $canonicalmethod;
+		for(my $j=0; $j < scalar(@attr) ; $j++){
+			if($attr[$j]->nodeName eq "Algorithm"){
+				$canonicalmethod = $attr[$j]->value;	
+			}
+		}
+		$self->{signedInfo} = $self->canonicalizeData($signInfoNode, $canonicalmethod);
+		#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "DEBUG: ".Dumper($self->{signedInfo}));
+		return $self->{signedInfo};
+	}
+	return;
 }
+
 
 sub canonicalizeData{
 	my $self = shift;
 	my ( $node, $canonicalmethod ) = @_; 
 	
 	my $exclusive = 0;
-    my $withComments = 0;
-    if ($canonicalmethod eq "http://www.w3.org/TR/2001/REC-xml-c14n-20010315") {    
-        $exclusive = 0;
-        $withComments = 0;
-    }
-    
-    if ($canonicalmethod eq "http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments") {
-        $withComments = 1;
-    }
-    
-    if ($canonicalmethod eq "http://www.w3.org/2001/10/xml-exc-c14n#") {
-        $exclusive = 1;
-    }
-    
-    if ($canonicalmethod eq "http://www.w3.org/2001/10/xml-exc-c14n#WithComments") {
-        $exclusive = 1;
-        $withComments = 1;
-    }
-    
-    if($exclusive){
-    	return $node->toStringEC14N($withComments);
-    }else{
-    	return $node->toStringC14N($withComments)
-    }
-    
-    return;
+	my $withComments = 0;
+	if ($canonicalmethod eq "http://www.w3.org/TR/2001/REC-xml-c14n-20010315") {    
+		$exclusive = 0;
+		$withComments = 0;
+	}
+	
+	if ($canonicalmethod eq "http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments") {
+		$withComments = 1;
+	}
+	
+	if ($canonicalmethod eq "http://www.w3.org/2001/10/xml-exc-c14n#") {
+		$exclusive = 1;
+	}
+	
+	if ($canonicalmethod eq "http://www.w3.org/2001/10/xml-exc-c14n#WithComments") {
+		$exclusive = 1;
+		$withComments = 1;
+	}
+	
+	if($exclusive){
+		return $node->toStringEC14N($withComments);
+	}else{
+		return $node->toStringC14N($withComments)
+	}
+	
+	return;
 }
+
 
 sub validate_reference{
 	my $self = shift;
 
+	if (!$self->{xml_as_dom}->isSameNode($self->{signature_node})) {
+		$self->{signature_node}->parentNode->removeChild($self->{signature_node});
+	}
 
-	
-    if (!$self->{xml_as_dom}->isSameNode($self->{signature_node})) {
-        $self->{signature_node}->parentNode->removeChild($self->{signature_node});
-    }
-
-	
 	my $xpath = XML::LibXML::XPathContext->new($self->{signature_node});
-    $xpath->registerNs("secdsig", "http://www.w3.org/2000/09/xmldsig#");
-    my $query = "./secdsig:SignedInfo/secdsig:Reference";
-    my @nodes = $xpath->findnodes($query);
-    if(scalar(@nodes)==0){
-    	return;
-    }
-    
-    $self->{validated_nodes} = ();
-     
-    for(my $i=0; $i < scalar(@nodes); $i++) {
-    	if(!$self->processRefNode($nodes[$i])){
-    		$self->{validated_nodes} = ();
-    		return;		
-    	}
-    }
-    
-    return 1;
+	$xpath->registerNs("secdsig", "http://www.w3.org/2000/09/xmldsig#");
+	my $query = "./secdsig:SignedInfo/secdsig:Reference";
+	my @nodes = $xpath->findnodes($query);
+	if(scalar(@nodes)==0){
+		return;
+	}
+	
+	$self->{validated_nodes} = ();
 
+	for(my $i=0; $i < scalar(@nodes); $i++) {
+		if(!$self->processRefNode($nodes[$i])){
+			$self->{validated_nodes} = ();
+			return;
+		}
+	}
+	
+	return 1;
 }
+
 
 sub processRefNode{
 	my $self = shift;
@@ -211,18 +214,18 @@ sub processRefNode{
 	
 	#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "SAML DEBUG: ". $ref_node);
 	my @attr = $ref_node->attributes();
-    my $node_id = "";
-    for(my $j=0; $j < scalar(@attr) ; $j++){
-    	if($attr[$j]->nodeName eq "URI"){
-    		$node_id = $attr[$j]->value;
-    		if(!(substr($node_id, 0, 1) eq "#")){
-    			# external url not supported
-    			return;
-    		}
-    		$node_id = substr($node_id, 1, length($node_id));
-    		#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "SAML DEBUG: ". $node_id);
-    	}
-    }
+	my $node_id = "";
+	for(my $j=0; $j < scalar(@attr) ; $j++){
+		if($attr[$j]->nodeName eq "URI"){
+			$node_id = $attr[$j]->value;
+			if(!(substr($node_id, 0, 1) eq "#")){
+				# external url not supported
+				return;
+			}
+			$node_id = substr($node_id, 1, length($node_id));
+			#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message  => "SAML DEBUG: ". $node_id);
+		}
+	}
 	
 	my $xpath = XML::LibXML::XPathContext->new($self->{xml_as_dom});
 	$xpath->registerNs("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
@@ -240,10 +243,7 @@ sub processRefNode{
 	$self->{validated_nodes}{$node_id} = $dataObject; 
 	
 	return 1;
-	
 }
-
-
 
 
 sub validateDigest(){
@@ -258,22 +258,22 @@ sub validateDigest(){
 	
 	my $data = $self->processTransforms($ref_node, $data_object);
 	my $calculatedDigValue = $self->calculateDigest($digestAlgorithm, $data);
-    
-    # remove = paddng characters
-    $calculatedDigValue =~ s/\n//g;
-    
-    my $query2 = "./secdsig:DigestValue";
-    my @nodes2 = $xpath->findnodes($query2, $ref_node);
-    my $digestValue = $nodes2[0]->textContent;
-    
-    # remove = paddng characters
-    $digestValue =~ s/\=//g;
-    
+		
+		# remove = paddng characters
+		$calculatedDigValue =~ s/\n//g;
+		
+		my $query2 = "./secdsig:DigestValue";
+		my @nodes2 = $xpath->findnodes($query2, $ref_node);
+		my $digestValue = $nodes2[0]->textContent;
+		
+		# remove = paddng characters
+		$digestValue =~ s/\=//g;
+		
 	#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "---". $data . "---");
 	
 	#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "SAML DEBUG: val hash is ". $calculatedDigValue);
 	#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "SAML DEBUG: xml hash is ". $digestValue);
-    
+		
 	
 	if($calculatedDigValue  eq $digestValue){
 		#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "SAML DEBUG: digest are matching");
@@ -289,7 +289,6 @@ sub processTransforms(){
 	my ( $ref_node, $data_object ) = @_;
 	
 	return $self->canonicalizeData($data_object, "http://www.w3.org/2001/10/xml-exc-c14n#");
-	
 }
 
 sub calculateDigest(){
@@ -328,14 +327,14 @@ sub locateKey(){
 	
 	
 	my $algorithm = $nodes[0]->textContent;
-    if ($algorithm) {
-    	#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "SAML DEBUG: ".$algorithm);
-    	$self->{key_sign_algo} = $algorithm;
-        # $objKey = new XMLSecurityKey($algorithm, array('type'=>'public'));
-        #return $objKey;
-    }
-	
+	if ($algorithm) {
+		#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "SAML DEBUG: ".$algorithm);
+		$self->{key_sign_algo} = $algorithm;
+		# $objKey = new XMLSecurityKey($algorithm, array('type'=>'public'));
+		#return $objKey;
+	}
 }
+
 
 sub verify_signature(){
 	my $self = shift;
@@ -350,47 +349,52 @@ sub verify_signature(){
 	
 	my $x509 = Crypt::OpenSSL::X509->new_from_string($self->{settings}{SSOx509Cert}, Crypt::OpenSSL::X509::FORMAT_PEM);
 	my $pubkey = Crypt::OpenSSL::RSA->new_public_key($x509->pubkey());
-	$pubkey->use_sha1_hash();
+	switch($self->{key_sign_algo}) {
+		case /rsa-sha256$/ { $pubkey->use_sha256_hash(); }
+		case /rsa-sha384$/ { $pubkey->use_sha384_hash(); }
+		case /rsa-sha512$/ { $pubkey->use_sha512_hash(); }
+		else { $pubkey->use_sha1_hash(); }
+	}
 
 	my $valid = $pubkey->verify($self->{signed_info}, $sigValue);
 	
 	#$Kernel::OM->Get('Kernel::System::Log')->Log(Priority => 'error', Message => "SAML DEBUG: Signature is ".$valid);
 	
 	return $valid;
-	
 }
+
 
 sub validateNumAssertions(){
 	my $self = shift;
-    
-    my @assertionNodes = $self->{xml_as_dom}->getElementsByTagName('saml:Assertion');
-    return (scalar(@assertionNodes) == 1);
+		
+	my @assertionNodes = $self->{xml_as_dom}->getElementsByTagName('saml:Assertion');
+	return (scalar(@assertionNodes) == 1);
 }
 
 
 sub validateTimestamps(){
 	my $self = shift;
-    
-    my @timestampNodes = $self->{xml_as_dom}->getElementsByTagName('saml:Conditions');
-    for(my $i=0; $i < scalar(@timestampNodes) ; $i++){
-    	my @attr = $timestampNodes[$i]->attributes();
-    	
-    	for(my $j=0; $j < scalar(@attr) ; $j++){
-    		if(
-    			$attr[$j]->nodeName eq "NotBefore" && 
-    			(DateTime::Format::DateParse->parse_datetime($attr[$j]->value) - DateTime->now())->is_positive()   
-    		){
-    			return;
-    		}
-    		if(
-    			$attr[$j]->nodeName eq "NotOnOrAfter" && 
-    			!(DateTime::Format::DateParse->parse_datetime($attr[$j]->value) - DateTime->now())->is_positive()   
-    		){
-    			return;
-    		}
-    	}
-    }
-    return 1;
+		
+	my @timestampNodes = $self->{xml_as_dom}->getElementsByTagName('saml:Conditions');
+	for(my $i=0; $i < scalar(@timestampNodes) ; $i++){
+		my @attr = $timestampNodes[$i]->attributes();
+		
+		for(my $j=0; $j < scalar(@attr) ; $j++){
+			if(
+				$attr[$j]->nodeName eq "NotBefore" && 
+				(DateTime::Format::DateParse->parse_datetime($attr[$j]->value) - DateTime->now())->is_positive()   
+			){
+				return;
+			}
+			if(
+				$attr[$j]->nodeName eq "NotOnOrAfter" && 
+				!(DateTime::Format::DateParse->parse_datetime($attr[$j]->value) - DateTime->now())->is_positive()   
+			){
+				return;
+			}
+		}
+	}
+	return 1;
 }
 
 
@@ -411,27 +415,25 @@ sub get_attribute() {
 	my $self = shift;
 	my ( $name ) = @_;
 	
-    my $xpath = XML::LibXML::XPathContext->new($self->{xml_as_dom});
+	my $xpath = XML::LibXML::XPathContext->new($self->{xml_as_dom});
 	$xpath->registerNs("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
 	$xpath->registerNs("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
 	my $query = '/samlp:Response/saml:Assertion/saml:AttributeStatement/saml:Attribute[@Name="'. $name .'"]/saml:AttributeValue';
 	my @entries = $xpath->findnodes($query);
-    
-    
-    if(scalar(@entries)> 0){
-        if(scalar(@entries) > 1) {
-            my @return = ();
-            for(my $i=0; $i < scalar(@entries); $i++) {
-                push(@return, $entries[$i]->textContent());
-            }
-            return @return;
-        } else {
-            return $entries[0]->textContent();
-            #return "mappolonia";
-        }
-    }else{
-        return ;
-    }
+
+	if(scalar(@entries)> 0){
+		if(scalar(@entries) > 1) {
+			my @return = ();
+			for(my $i=0; $i < scalar(@entries); $i++) {
+					push(@return, $entries[$i]->textContent());
+			}
+			return @return;
+		} else {
+			return $entries[0]->textContent();
+		}
+	}else{
+		return ;
+	}
 }
 
 1;
--- README.md
+++ README.md
@@ -7,7 +7,8 @@ This addon does not process to the auto-provisionning of the accounts, it means 
 The main advantage of this auth provider is that it can be chained with other auth providers so it is possible to have agents authenticated via SSO and others via a fallback Auth provider (otrs DB, Radius...)  
 Another advantage is that it does not depend on specific Apache auth modules.
 
-### Requirements:
+### Requirements
+
 DateTime;  
 DateTime::Format::DateParse;  
 MIME::Base64;  
@@ -17,33 +18,36 @@ Crypt::OpenSSL::RSA;
 Crypt::OpenSSL::X509;  
 Compress::Zlib;  
 URI::Escape;  
+Switch;  
 
+### Installation
 
-### Installation:
-  You can download the latest opm build on the dist folder of this repo and then install it using the web based addon manager.  
-  Alternatively you can  download the source files and copy them to ./Kernel/System/Auth folder of your OTRS installation.
-  
-### Notes on configuration:
+You can download the latest opm build on the dist folder of this repo and then install it using the web based addon manager.  
+Alternatively you can  download the source files and copy them to ./Kernel/System/Auth folder of your OTRS installation.
 
-  In order to display a "Login via SAML" (mandatory) button you have to include the html following code into the Login.tt template file, below or above the login form Kernel/Output/HTML/Templates/Standard/Login.tt
-  ```html
-  <!-- Add the sso login button -->
-  <div class="Content" style="text-align: center; display: block; height: 40px; padding-top: 20px; padding-bottom: 0; vertical-align: bottom; line-height: 40px;">
-    <a href="./index.pl?Action=Login&WantSAMLRequest=true" style="background: #e9e9e9; border-radius: 5px; padding: 10px;border: 1px solid #ccc; font-weight: bold;" >Login via SAML2 SSO</a>
-  </div>
-  <!-- end sso login button -->
-  ```
+### Notes on configuration
 
-  In the Default.pm you can activate this auth module with a fallback on the default DB module
+In order to display a "Login via SAML" (mandatory) button you have to include the html following code into the Login.tt template file, below or above the login form Kernel/Output/HTML/Templates/Standard/Login.tt
 
-  ```perl
-  $Self->{AuthModule1} = 'Kernel::System::Auth::DB';
-  $Self->{AuthModule} = 'Kernel::System::Auth::SAML2SP';
-  $Self->{'AuthModule::SAML2SP::SSOIdpUrl'} = 'https://idp.example.com/simplesaml/saml2/idp/SSOService.php';
-  $Self->{'AuthModule::SAML2SP::SSOAssertionConsumerURL'} = '/otrs/index.pl?Action=Login';
-  $Self->{'AuthModule::SAML2SP::SSOUserIdAttribute'} = 'UserId';
-  $Self->{'AuthModule::SAML2SP::SSOx509Cert'} = "-----BEGIN CERTIFICATE-----[certificate content here can spread on many lines]-----END CERTIFICATE-----";
+```html
+<!-- Add the sso login button -->
+<div class="Content" style="text-align: center; display: block; height: 40px; padding-top: 20px; padding-bottom: 0; vertical-align: bottom; line-height: 40px;">
+  <a href="./index.pl?Action=Login&WantSAMLRequest=true" style="background: #e9e9e9; border-radius: 5px; padding: 10px;border: 1px solid #ccc; font-weight: bold;" >Login via SAML2 SSO</a>
+</div>
+<!-- end sso login button -->
+```
+
+In the Default.pm you can activate this auth module with a fallback on the default DB module
+
+```perl
+$Self->{AuthModule1} = 'Kernel::System::Auth::DB';
+$Self->{AuthModule} = 'Kernel::System::Auth::SAML2SP';
+$Self->{'AuthModule::SAML2SP::SSOIdpUrl'} = 'https://idp.example.com/simplesaml/saml2/idp/SSOService.php';
+$Self->{'AuthModule::SAML2SP::SSOAssertionConsumerURL'} = '/otrs/index.pl?Action=Login';
+$Self->{'AuthModule::SAML2SP::SSOUserIdAttribute'} = 'UserId';
+$Self->{'AuthModule::SAML2SP::SSOx509Cert'} = "-----BEGIN CERTIFICATE-----[certificate content here can spread on many lines]-----END CERTIFICATE-----";
 ```
 
 ### Credits
+
 This addons is highly inspired by the php's [xmlseclibs](https://github.com/robrichards/xmlseclibs) by Rob Richards
